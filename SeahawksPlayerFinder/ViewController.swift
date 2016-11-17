cccbb5c5ddab8c3e04edcad50f2ffd12abd6210d
Connor Bradley <s13169296@cn1103n1-lw0024.staff.uce.ac.uk>
2016-11-17 14:43:56 +0000

    Initial Commit

---
+++ SeahawksPlayerFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeahawksPlayerFinder
+//
+//  Created by Connor Bradley on 17/11/2016.
+//  Copyright © 2016 Connor Bradley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
